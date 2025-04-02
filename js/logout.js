1bf51e097bd80395608dadc123838b9e2c3be233
Alex Wauquier <alex@alexwauquier.com>
2025-04-02 16:19:43 +0200

    style: fix indentation in JavaScript files

--- js/logout.js
+++ js/logout.js
@@ -1,23 +1,23 @@
 document.getElementById("logout-btn").addEventListener("click", function() {
-    logout();
+  logout();
 });
 
 // Fonction de déconnexion
 function logout() {
-    // Supprimer le token
-    localStorage.removeItem("token");
+  // Supprimer le token
+  localStorage.removeItem("token");
 
-    // Afficher une alerte avec SweetAlert2
-    Swal.fire({
-        title: "Successful logout",
-        text: "You have been disconnected successfully.",
-        icon: "success",
-        timer: 1500, // La notification disparaît après 1.5 seconde
-        showConfirmButton: false,
-    });
+  // Afficher une alerte avec SweetAlert2
+  Swal.fire({
+    title: "Successful logout",
+    text: "You have been disconnected successfully.",
+    icon: "success",
+    timer: 1500, // La notification disparaît après 1.5 seconde
+    showConfirmButton: false,
+  });
 
-    // Rediriger vers la page de connexion après un court délai
-    setTimeout(() => {
-        window.location.href = "../index.html";
-    }, 1500);
+  // Rediriger vers la page de connexion après un court délai
+  setTimeout(() => {
+    window.location.href = "../index.html";
+  }, 1500);
 }
