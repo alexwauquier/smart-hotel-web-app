35f2eca38ff3800657977052e232bb3ac4d4d1e8
Alex Wauquier <alex@alexwauquier.com>
2025-04-28 23:21:23 +0200

    style: fix indentation in JS files

--- js/notification.js
+++ js/notification.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const notifBtn = document.querySelector('.notification');
-    const notifMenu = document.getElementById('notif-menu');
-  
-    notifBtn.addEventListener('click', (e) => {
-      e.stopPropagation();
-      notifMenu.classList.toggle('hidden');
-    });
-  
-    document.addEventListener('click', (e) => {
-      if (!notifMenu.contains(e.target) && !notifBtn.contains(e.target)) {
-        notifMenu.classList.add('hidden');
-      }
-    });
+  const notifBtn = document.querySelector('.notification');
+  const notifMenu = document.getElementById('notif-menu');
+
+  notifBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    notifMenu.classList.toggle('hidden');
   });
-  
+
+  document.addEventListener('click', (e) => {
+    if (!notifMenu.contains(e.target) && !notifBtn.contains(e.target)) {
+      notifMenu.classList.add('hidden');
+    }
+  });
+});
